a0c12834d3393358c6018ea2052422540352100b
Kwe <Kweayon@gmail.com>
2023-03-29 17:18:23 -0500

    Upgraded Android Project

--- android/app/src/androidTest/java/com/resocoder/integration_test_tutorial/MainActivityTest.java
+++ android/app/src/androidTest/java/com/resocoder/integration_test_tutorial/MainActivityTest.java
@@ -1,12 +1,14 @@
 // Change this line to YOUR package name:
 package com.resocoder.integration_test_tutorial;
-
+import io.flutter.embedding.android.FlutterActivity;
 import androidx.test.rule.ActivityTestRule;
 import dev.flutter.plugins.integration_test.FlutterTestRunner;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 // Import MainActivity from YOUR package
 import com.resocoder.integration_test_tutorial.MainActivity;
+
+public class MainActivity extends FlutterActivity { }
 
 @RunWith(FlutterTestRunner.class)
 public class MainActivityTest {
